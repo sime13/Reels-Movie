83b6785c6e5d497f7ad2c4c423d38935d0fe08d0
sime13 <a.simus@yahoo.com>
2024-03-19 15:24:36 -0300

    segundo commit

---
+++ ReelsMovies/ReelsMovies/Data/AplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ReelsMovies.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<  >  { get; set; }
+    }
+
+}
--- ReelsMovies/ReelsMovies/obj/Debug/net8.0/ReelsMovies.AssemblyInfo.cs
+++ ReelsMovies/ReelsMovies/obj/Debug/net8.0/ReelsMovies.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ReelsMovies")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d43ffd2c47be5c871537f81ab4ab3e5a1f6b6437")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24a4259483a25e7fa49e2ac0cd5f33e42de73965")]
 [assembly: System.Reflection.AssemblyProductAttribute("ReelsMovies")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ReelsMovies")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
